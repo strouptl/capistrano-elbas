c38ce11be574a267c9eaef3583d9a1c0fb5390ac
Laney Stroup <laney@stroupsolutions.com>
2024-03-28 10:03:54 +0900

    add ability to utilize private IP Addresses for deploying from within AWS VPC;

--- lib/elbas/aws/instance.rb
+++ lib/elbas/aws/instance.rb
@@ -12,31 +12,21 @@ module Elbas
         @aws_counterpart = aws_namespace::Instance.new id, client: aws_client
       end
 
-      def public_dns_hostname
-        aws_counterpart.public_dns_name
-      end
-
-      def ip_address
-        aws_counterpart.public_ip_address
-      end
-
-      def ipv_6_address
-        aws_counterpart.ipv_6_address
-      end
-
       def formatted_ipv_6_address
-        "[#{ipv_6_address}]"
+        "[#{aws_counterpart.ipv_6_address}]"
       end
 
       def auto_destination
-        [public_dns_hostname, ip_address, formatted_ipv_6_address].detect { |a| !a.nil? and !a.empty? }
+        (options[:private] ? private_destination_options : public_destination_options).detect { |a| !a.nil? and !a.empty? }
       end
 
       def destination
         @destination ||= if options[:ipv_4]
-                           ip_address
+                           aws_counterpart.public_ip_address
                          elsif options[:ipv_6]
                            formatted_ipv_6_address
+                         elsif options[:private_ip_address]
+                           aws_counterpart.private_ip_address
                          else
                            auto_destination
                          end
@@ -49,6 +39,14 @@ module Elbas
       private
         def aws_namespace
           ::Aws::EC2
+        end
+
+        def private_destination_options
+          [aws_counterpart.private_dns_name, aws_counterpart.private_ip_address]
+        end
+
+        def public_destination_options
+          [aws_counterpart.public_dns_name, aws_counterpart.public_ip_address, formatted_ipv_6_address]
         end
     end
   end
